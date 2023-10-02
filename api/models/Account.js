0cc09d5679e3c7be9c4ef963e3df909bd562aa03
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-02 17:01:32 -0400

    account model

---
+++ api/models/Account.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+    const Account = sequelize.define('Account', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+            readonly: true,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+        }, 
+        last_name: {
+            type: DataTypes.STRING,
+        },
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            validate: {
+                isEmail: {
+                    msg: "Email address must be valid"
+                },
+            },
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }, {
+        createdAt: 'account_created',
+        updatedAt: 'account_updated',
+    });
+
+    return Account;
+}
