a1cd190069a239637a2126eb0697410be7b44e6a
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-02 18:44:34 -0400

    assignment model

---
+++ api/models/Assignment.js
@@ -0,0 +1,39 @@
+export const AssignmentModel = (sequelize, DataTypes) => {
+    const Assigment = sequelize.define('Account', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            readonly: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }, 
+        points: {
+            type: DataTypes.INTEGER,
+            validate: {
+                min: 0,
+                max: 100,
+            },
+            allowNull: false,
+        },
+        num_of_attempts: {
+            type: DataTypes.INTEGER,
+            validate: {
+                min: 0,
+                max: 100,
+            },
+            allowNull: false,
+        },
+        deadline: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        }
+    }, {
+        createdAt: 'account_created',
+        updatedAt: 'account_updated',
+    });
+
+    return Assigment;
+}
