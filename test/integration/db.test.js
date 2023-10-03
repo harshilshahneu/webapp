eb501e9fcd538e2e4e554a31133b1eb3028a34f9
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-03 16:27:58 -0400

    adding test case and jest

---
+++ test/integration/db.test.js
@@ -0,0 +1,8 @@
+import { getConnection } from "../../api/services/healthz-service";
+
+describe("Database connection", () => {
+    test('should check the connection', async () => { 
+        const connection = await getConnection();
+        expect(connection.status).toBe(200);
+     })
+});
