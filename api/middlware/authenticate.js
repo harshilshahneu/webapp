b008ac62a729721e4ea9deb8a0a782923d062285
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-04 04:11:05 -0400

    code cleanup auth middleware

--- api/middlware/authenticate.js
+++ api/middlware/authenticate.js
@@ -2,28 +2,32 @@ import * as accountService from '../services/account-service.js';
 import { setResponse } from '../utils/response-utils.js';
 
 export const authenticate = async (req, res, next) => {
-    // parse login and password from headers
-    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
-    const [email, password] = Buffer.from(b64auth, 'base64').toString().split(':');
-    
-    // Verify login and password are set and correct
-    if (email && password) {
-        //check if login and password are in the database
-        const AccountId = await accountService.getByLogin({ email, password });
-        if(AccountId) {
-            // Access granted...
-            req.user = { email, AccountId };
-            return next();
+    try {
+        // parse login and password from headers
+        const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+        const [email, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+        
+        // Verify login and password are set and correct
+        if (email && password) {
+            //check if login and password are in the database
+            const AccountId = await accountService.getByLogin({ email, password });
+            if(AccountId) {
+                // Access granted...
+                req.user = { email, AccountId };
+                return next();
+            }
         }
+    
+        // Access denied...
+        // if the email and password are not set, challenge the client to authenticate
+        // if the email and password are set, but incorrect, send only 401
+        const authenticateHeader = email && password ? {} : { "WWW-Authenticate": 'Basic realm="Access to the staging site", charset="UTF-8"' };
+        setResponse(
+            res,
+            401,
+            authenticateHeader
+        )
+    } catch (err) {
+        setResponse(res, 400, null, err);
     }
-
-    // Access denied...
-    // if the email and password are not set, challenge the client to authenticate
-    // if the email and password are set, but incorrect, send only 401
-    const authenticateHeader = email && password ? {} : { "WWW-Authenticate": 'Basic realm="Access to the staging site", charset="UTF-8"' };
-    setResponse(
-        res,
-        401,
-        authenticateHeader
-    )
 }
--- api/services/account-service.js
+++ api/services/account-service.js
@@ -1,23 +1,18 @@
 import { Account } from "../sequelize.js";
 import { validatePassword } from "../utils/bcrypt-utils.js";
 
-//get all the accounts
-//@TODO clean th catch block
+//get and validate account by email and password
 export const getByLogin = async ({email, password}) => {
-    try {
-        let AccountId = null;
-        const account = await Account.findOne({
-            where: {
-                email,
-            },
-        });
+    let AccountId = null;
+    const account = await Account.findOne({
+        where: {
+            email,
+        },
+    });
 
-        if(account && (await validatePassword(password, account.password))) {
-            AccountId = account.id;
-        }
-       
-        return AccountId;
-    } catch (err) {
-        return "";
+    if(account && (await validatePassword(password, account.password))) {
+        AccountId = account.id;
     }
+       
+    return AccountId;
 }
