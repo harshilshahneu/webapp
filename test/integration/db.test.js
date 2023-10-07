b79e0585c7595b1735073e2be8ba6b1ddc35f50b
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-07 16:03:27 -0400

    updated test

--- test/integration/db.test.js
+++ test/integration/db.test.js
@@ -3,6 +3,6 @@ import { getConnection } from "../../api/services/healthz-service";
 describe("Database connection", () => {
     test('should check the connection', async () => { 
         const connection = await getConnection();
-        expect(connection.status).toBe(200);
+        expect(connection.status).toBe(201);
      })
 });
