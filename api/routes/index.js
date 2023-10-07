c9ab7da10288099f7969d06fa28bee59ed3ba217
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-07 15:56:55 -0400

    updated route name

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,7 +7,7 @@ import { getDBStatus } from '../middlware/check-db-health.js';
 //Pass the app to the routes
 const routes = (app) => {
     //Health check route
-    app.use('/healthz', healthzRoutes);
+    app.use('/health', healthzRoutes);
     
     //Assignment routes
     // check the db status -> authenticate -> validate the payload -> call the controller
