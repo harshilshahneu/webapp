8804b0b21ef1b1e72260bada7fa64bdbd7da5a6d
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-18 10:32:08 -0400

    make status code same for different db

--- api/controllers/healthz-controller.js
+++ api/controllers/healthz-controller.js
@@ -5,17 +5,7 @@ export const get = async (req, res) => {
     try {
         const { status } = await healthZService.getConnection();
     
-        switch(status) {
-            case 200:
-                setResponse(res, 200);
-                break;
-            case -61:
-                setResponse(res, 503);
-                break;
-            default:
-                setResponse(res, 400);
-                break;
-        }
+        setResponse(res, status === 200 ? 200 : 503)
     } catch (err) {
         setResponse(res, 400);
     }
