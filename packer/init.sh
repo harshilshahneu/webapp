37cbb3219167e1d7e0f804ebbccdee6e862452bd
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-24 18:35:01 -0400

    adding user group and chaning dir to /opt

--- packer/init.sh
+++ packer/init.sh
@@ -21,21 +21,13 @@ echo "installing npm"
 sudo apt-get install -y npm
 npm -v
 
-#install mariadb
-# echo "installing mariadb"
-# sudo apt install -y mariadb-server
-
 #make and folder unzip the project
+cd /opt || exit
 sudo mkdir webapp
 sudo unzip webapp.zip -d webapp
 
 #remove .git
 sudo apt-get remove -y git
-
-#setup db
-# echo "GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;" | sudo mariadb
-# echo "FLUSH PRIVILEGES;" | sudo mariadb
-# echo "CREATE DATABASE HealthConnectDB;" | sudo mariadb
 
 # go in the webapp
 cd webapp/ || exit
--- packer/setup.sh
+++ packer/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# Add user and group
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+
+# Move the service file to systemd
 sudo cp /tmp/bootup.service /lib/systemd/system/bootup.service
+
+# Enable and start the service
+systemctl daemon-reload
 sudo systemctl enable bootup
 sudo systemctl start bootup
-# sudo systemctl restart bootup
 sudo systemctl status bootup
