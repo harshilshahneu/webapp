3aabbc9334d70353bf8ef59932fec20b265dd5a1
Harshil Shah <shah.harshil2@northeastern.edu>
2023-10-25 17:18:33 -0400

    changing user permissions

--- packer/setup.sh
+++ packer/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#change ownership
+sudo chown -R csye6225:csye6225 /opt/csye6225/webapp
+sudo chmod -R 750  /opt/csye6225/webapp
+
 # Move the service file to systemd
 sudo cp /tmp/bootup.service /lib/systemd/system/bootup.service
 
